220c8683385e87faa97bb983603de502bdf5cf95
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2024-07-14 19:52:13 -0300

    Corrección de imports

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # Importar función para permitir el render de los templates
-from flask import render_template, request, redirect, flash, url_for
+from flask import render_template, request, redirect
 
 # Conexión la base de datos en mysql
 from flask_mysqldb import MySQL
